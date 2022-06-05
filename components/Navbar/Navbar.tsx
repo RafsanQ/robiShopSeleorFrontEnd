53192f5bd23b1a02f909651f263c3db096c2c658
Rafsan <rafsan.quayes@gmail.com>
2022-06-05 16:39:37 +0600

    Logo at the centre

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -50,14 +50,16 @@ export function Navbar() {
           <div className="flex justify-between h-16">
             <div className="flex justify-between items-center">
               <MainMenu />
-              <Link href={paths.$url()} passHref>
+            </div>
+
+            <Link href={paths.$url()} passHref>
                 <a href=" pass">
                   <div className="mt-5% group block h-16 w-28 relative p-3 pt-4">
                     <Image src="/logos/robiShopLogo.png" alt="Saleor logo" layout="responsive" width={600} height={200}/>
                   </div>
                 </a>
-              </Link>
-            </div>
+            </Link>
+
             <div className="flex space-x-px md:space-x-8 items-center">
               <Link href={paths.search.$url()} passHref>
                 <a href="pass" className="-m-2 p-2 flex items-center" aria-label="Search">
